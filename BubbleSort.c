37780e978fac317dea1c30d08f5ea7b05a76724a
devpandhii <devpandhi1@gmail.com>
2021-10-03 13:10:16 +0530

    2nd commit

--- BubbleSort.c
+++ BubbleSort.c
@@ -18,7 +18,7 @@ int main()
   {
     for (d = 0 ; d < n - c - 1; d++)
     {
-      if (array[d] > array[d+1]) /* For decreasing order use '<' instead of '>' */
+      if (array[d] > array[d+1]) 
       {
         swap       = array[d];
         array[d]   = array[d+1];
